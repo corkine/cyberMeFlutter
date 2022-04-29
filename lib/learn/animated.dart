c87f8a3f571bb7bc9b3844181a92c7effdfe5ff5
corkine <corkine@outlook.com>
2022-04-29 23:27:27 +0800

    add rive learn

--- lib/learn/animated.dart
+++ lib/learn/animated.dart
@@ -1,4 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:rive/rive.dart';
 
 class LearnAnimation extends StatelessWidget {
   const LearnAnimation({Key? key}) : super(key: key);
@@ -7,8 +11,132 @@ class LearnAnimation extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyAni(),
+      home: HelloRive(),
     );
+  }
+}
+
+class HelloRive extends StatefulWidget {
+  const HelloRive({Key? key}) : super(key: key);
+
+  @override
+  State<HelloRive> createState() => _HelloRiveState();
+}
+
+class _HelloRiveState extends State<HelloRive> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: const Center(
+          child: RiveAnimation.asset("images/bird.riv",
+          animations: ['idle'],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+class HelloTicker extends StatefulWidget {
+  const HelloTicker({Key? key}) : super(key: key);
+
+  @override
+  State<HelloTicker> createState() => _HelloTickerState();
+}
+
+class _HelloTickerState extends State<HelloTicker>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  List<Snowflake> snowList = List.generate(100, (index) => Snowflake());
+
+  @override
+  void initState() {
+    controller =
+        AnimationController(duration: const Duration(seconds: 4), vsync: this)
+          ..repeat();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          decoration: const BoxDecoration(
+            color: Colors.blue,
+            /*gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Colors.blue, Colors.blueAccent, Colors.white],
+              stops: [0, 0.8, 0.95]
+            )*/
+          ),
+          height: double.infinity,
+          width: double.infinity,
+          child: AnimatedBuilder(
+            animation: controller,
+            builder: (context, child) {
+              for (var element in snowList) {
+                element.fall();
+              }
+              return CustomPaint(
+                painter: MyPainter(snowList),
+              );
+            },
+          )),
+    );
+  }
+}
+
+class MyPainter extends CustomPainter {
+  final List<Snowflake> snowList;
+
+  MyPainter(this.snowList);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()..color = Colors.white;
+    canvas.drawOval(
+        Rect.fromCenter(
+            center: size.center(const Offset(0, 210)), width: 90, height: 90),
+        paint);
+    canvas.drawOval(
+        Rect.fromCenter(
+            center: size.center(const Offset(0, 340)), width: 150, height: 200),
+        paint);
+    for (var s in snowList) {
+      canvas.drawCircle(Offset(s.x, s.y), s.r, paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class Snowflake {
+  double x = Random().nextDouble() * 400;
+  double y = Random().nextDouble() * 600;
+  double r = Random().nextInt(3) + 1;
+  double v = Random().nextDouble() * 4 + 2;
+
+  fall() {
+    y += v;
+    if (y > 800) {
+      y = 0;
+      x = Random().nextDouble() * 400;
+      r = Random().nextInt(4) + 1;
+      v = Random().nextDouble() * 4 + 2;
+    }
   }
 }
 
@@ -78,19 +206,19 @@ class _MyAniState extends State<MyAni> with TickerProviderStateMixin {
                   return Container(
                     width: 300,
                     height: 300,
-                    decoration: BoxDecoration(
+                    /*decoration: BoxDecoration(
                         shape: BoxShape.circle,
                         color: Colors.blue,
                         gradient: RadialGradient(colors: [
                           Colors.blue[600]!,
                           Colors.blue[100]!
-                        ], stops: /*controller.value <= 0.2
+                        ], stops: *//*controller.value <= 0.2
                                 ? [a1.value, a1.value + 0.1]
-                                : [a3.value, a3.value + 0.1]*/
+                                : [a3.value, a3.value + 0.1]*//*
                             [
                           controller.value,
                           controller.value + 0.1
-                        ])),
+                        ])),*/
                   );
                 }),
           ),
