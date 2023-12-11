be40fe39bae790f77282337dd913973cdc7f5359
mazj <mazhangjing@inspur.com>
2023-12-11 09:44:11 +0800

    Blog RSS 增加搜索功能

--- lib/pocket/app/blog.dart
+++ lib/pocket/app/blog.dart
@@ -1,4 +1,5 @@
 import 'package:cyberme_flutter/api/blog.dart';
+import 'package:cyberme_flutter/pocket/app/statistics.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -12,48 +13,92 @@ class BlogView extends ConsumerStatefulWidget {
 }
 
 class _BlogViewState extends ConsumerState<BlogView> {
+  final search = TextEditingController();
+
+  @override
+  void dispose() {
+    search.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final blogs = ref.watch(fetchBlogsProvider).value;
+    var blogs = ref.watch(fetchBlogsProvider).value;
     Widget content;
     if (blogs == null) {
       content = const Center(child: CupertinoActivityIndicator());
     } else {
-      content = RefreshIndicator(
-        onRefresh: () async {
-          final _ = await ref.refresh(fetchBlogsProvider.future);
-        },
-        child: ListView.builder(
-            itemCount: blogs.length,
-            itemBuilder: (c, i) => ListTile(
-                title: Text(blogs[i].title),
-                subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(blogs[i].summary,
-                          style:
-                              const TextStyle(fontSize: 12, color: Colors.grey),
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis),
-                      const SizedBox(height: 4),
-                      Text(
-                          (blogs[i].author?.name ?? "") +
-                              " @ " +
-                              blogs[i].date_modified.substring(0, 10),
-                          style: const TextStyle(fontSize: 12))
-                    ]),
-                onTap: () {
-                  launchUrlString(blogs[i].url);
-                })),
-      );
+      final searchText = search.text.toUpperCase();
+      blogs = blogs.where((e) {
+        if (searchText.isEmpty) return true;
+        return e.title.toUpperCase().contains(searchText) ||
+            e.summary.toUpperCase().contains(searchText);
+      }).toList();
+      content = SafeArea(
+          child: Column(children: [
+        Expanded(
+            child: RefreshIndicator(
+          onRefresh: () async {
+            final _ = await ref.refresh(fetchBlogsProvider.future);
+          },
+          child: ListView.builder(
+              itemCount: blogs.length,
+              itemBuilder: (c, i) => ListTile(
+                  title: Text(blogs![i].title),
+                  subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(blogs[i].summary,
+                            style: const TextStyle(
+                                fontSize: 12, color: Colors.grey),
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis),
+                        const SizedBox(height: 4),
+                        Text(
+                            (blogs![i].author?.name ?? "") +
+                                " @ " +
+                                blogs[i].date_modified.substring(0, 10),
+                            style: const TextStyle(fontSize: 12))
+                      ]),
+                  onTap: () {
+                    launchUrlString(blogs![i].url);
+                  })),
+        )),
+        Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CupertinoTextField(
+                placeholder: "Search",
+                controller: search,
+                style: TextStyle(
+                    color: Theme.of(context).colorScheme.onInverseSurface),
+                onEditingComplete: () => setState(() {}),
+                suffix: IconButton(
+                    icon: const Icon(CupertinoIcons.xmark,
+                        color: Colors.grey, size: 18),
+                    onPressed: () {
+                      search.clear();
+                      setState(() {});
+                    })))
+      ]));
     }
-    return Scaffold(
-        appBar: AppBar(
-            title: const Column(children: [
-          Text('Blogs Feed'),
-          Text('blogs.mazhangjing.com',
-              style: TextStyle(fontSize: 10, fontFamily: "mono"))
-        ])),
-        body: content);
+    return Theme(
+      data: themeData,
+      child: Scaffold(
+          appBar: AppBar(
+              title: const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Blogs Feed'),
+                    Text('www.mazhangjing.com/blog',
+                        style: TextStyle(fontSize: 10, fontFamily: "mono")),
+                  ]),
+              actions: [
+                IconButton(
+                    icon: const Icon(Icons.link),
+                    onPressed: () =>
+                        launchUrlString("https://www.mazhangjing.com/blogs"))
+              ]),
+          body: content),
+    );
   }
 }
